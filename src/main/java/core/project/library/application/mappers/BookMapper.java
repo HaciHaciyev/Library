42b4137a3a8cfa6a99e40cdbdb31fa202fee5dd6
AinGrace <qnicat32@gmail.com>
2024-05-04 14:01:58 +0400

    Partially implement DTO and Mapper

--- src/main/java/core/project/library/application/mappers/BookMapper.java
+++ src/main/java/core/project/library/application/mappers/BookMapper.java
@@ -1,5 +1,11 @@
 package core.project.library.application.mappers;
 
+import core.project.library.application.model.BookDTO;
+import core.project.library.domain.entities.Book;
+import org.mapstruct.Mapper;
+
 //TODO for Nicat
+@Mapper
 public interface BookMapper {
+    BookDTO toDTO(Book book);
 }
--- src/main/java/core/project/library/application/model/BookDTO.java
+++ src/main/java/core/project/library/application/model/BookDTO.java
@@ -1,5 +1,28 @@
 package core.project.library.application.model;
 
+import core.project.library.domain.entities.Author;
+import core.project.library.domain.entities.Order;
+import core.project.library.domain.entities.Publisher;
+import core.project.library.domain.events.Events;
+import core.project.library.domain.value_objects.Category;
+import core.project.library.domain.value_objects.Description;
+import core.project.library.domain.value_objects.ISBN;
+import core.project.library.domain.value_objects.Title;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
 //TODO for Nicat
 public class BookDTO {
+    private @NotNull Title title;
+    private @NotNull Description description;
+    private @NotNull ISBN isbn;
+    private @NotNull BigDecimal price;
+    private @NotNull Integer quantityOnHand;
+    private @NotNull Category category;
+    private @NotNull Events events;
+    private /**@ManyToOne*/Publisher publisher;
+    private /**@ManyToMany*/Set<Author> authors;
+    private /**@ManyToMany*/Set<Order> orders;
 }
