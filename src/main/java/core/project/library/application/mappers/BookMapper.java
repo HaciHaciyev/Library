fc03252084985785af2557522df093be3134729f
AinGrace <qnicat32@gmail.com>
2024-05-04 14:01:58 +0400

    Partially implement DTO and Mapper

--- src/main/java/core/project/library/application/mappers/BookMapper.java
+++ src/main/java/core/project/library/application/mappers/BookMapper.java
@@ -1,5 +1,14 @@
 package core.project.library.application.mappers;
 
+import core.project.library.application.model.BookDTO;
+import core.project.library.domain.entities.Book;
+import org.mapstruct.Mapper;
+
 //TODO for Nicat
+@Mapper
 public interface BookMapper {
+
+    BookDTO toDTO(Book book);
+
+    Book toBook(BookDTO bookDTO);
 }
