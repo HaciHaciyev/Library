12ed5ce0f296579ed86a08d0245b56b9e71de31d
Ain Grace <qnicat32@gmail.com>
2024-06-07 17:52:24 +0400

    Feat: refine bootstrap

--- src/main/java/core/project/library/infrastructure/bootstrap/Bootstrap.java
+++ src/main/java/core/project/library/infrastructure/bootstrap/Bootstrap.java
@@ -1,26 +1,187 @@
 package core.project.library.infrastructure.bootstrap;
 
+import core.project.library.domain.entities.*;
+import core.project.library.domain.events.Events;
 import core.project.library.infrastructure.repository.BootstrapRepository;
 import lombok.extern.slf4j.Slf4j;
+import net.datafaker.Faker;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static core.project.library.infrastructure.utilities.ValueObjects.*;
 
 @Slf4j
 @Component
 public class Bootstrap implements CommandLineRunner {
 
+    private static final Faker faker = new Faker();
+
+    private final int MAX_NUMBER_OF_AUTHORS = faker.number().numberBetween(1, 15);
+    private final int MAX_NUMBER_OF_AUTHORS_PER_BOOK = faker.number().numberBetween(1, 4);
+    private final int MAX_NUMBER_OF_PUBLISHERS = faker.number().numberBetween(1, 5);
+    private final int MAX_NUMBER_OF_CUSTOMERS = faker.number().numberBetween(1, 10);
+    private final int MAX_NUMBER_OF_BOOKS = faker.number().numberBetween(1, 30);
+    private final int MAX_NUMBER_OF_ORDERS = faker.number().numberBetween(1, 15);
+    private final int MAX_COUNT_OF_BOOKS_FOR_ORDER = faker.number().numberBetween(1, 10);
+
+    private List<Publisher> publishers;
+    private List<Author> authors;
+    private List<Book> books;
+    private List<Customer> customers;
+    private List<Order> orders;
+
     private final BootstrapRepository repository;
 
     public Bootstrap(BootstrapRepository repository) {
+        populatePublishers();
+        populateAuthors();
+        populateBooks();
+        populateCustomers();
+        populateOrders();
         this.repository = repository;
     }
 
     @Override
     public final void run(String... args) throws Exception {
-        if (repository.count() < 1) {
-            repository.bootstrap();
+
+    }
+
+    private Supplier<Publisher> publisherSupplier() {
+        return () -> Publisher.builder()
+                .id(UUID.randomUUID())
+                .publisherName(randomPublisherName())
+                .address(randomAddress())
+                .phone(randomPhone())
+                .email(randomEmail())
+                .events(new Events())
+                .build();
+    }
+
+    private Supplier<Author> authorSupplier() {
+        return () -> Author.builder()
+                .id(UUID.randomUUID())
+                .firstName(randomFirstName())
+                .lastName(randomLastName())
+                .email(randomEmail())
+                .address(randomAddress())
+                .events(new Events())
+                .build();
+    }
+
+    private Supplier<Book> bookSupplier() {
+        return () -> {
+            double randomPrice = faker.number().randomDouble(2, 1, 100);
+            int randomQuantity = faker.number().numberBetween(1, 15);
+            int randomPublisher = faker.number().numberBetween(0, MAX_NUMBER_OF_PUBLISHERS);
+            Set<Author> authorsForBook = getAuthorsForBook();
+
+            return Book.builder()
+                    .id(UUID.randomUUID())
+                    .title(randomTitle())
+                    .description(randomDescription())
+                    .isbn(randomISBN13())
+                    .price(BigDecimal.valueOf(randomPrice))
+                    .quantityOnHand(randomQuantity)
+                    .events(new Events())
+                    .category(randomCategory())
+                    .publisher(publishers.get(randomPublisher))
+                    .authors(authorsForBook)
+                    .build();
+        };
+    }
+
+    private Supplier<Customer> customerSupplier() {
+        return () -> Customer.builder()
+                .id(UUID.randomUUID())
+                .firstName(randomFirstName())
+                .lastName(randomLastName())
+                .password(randomPassword())
+                .email(randomEmail())
+                .address(randomAddress())
+                .events(new Events())
+                .build();
+    }
+
+    private Supplier<Order> orderSupplier() {
+        return () -> {
+            int randomCustomer = faker.number().numberBetween(0, customers.size());
+            int countOfBooks = 1;
+
+            if (MAX_COUNT_OF_BOOKS_FOR_ORDER > 1) {
+                countOfBooks = faker.number().numberBetween(1, MAX_COUNT_OF_BOOKS_FOR_ORDER);
+            }
+
+            return Order.builder()
+                    .id(UUID.randomUUID())
+                    .countOfBooks(countOfBooks)
+                    .totalPrice(randomTotalPrice())
+                    .events(new Events())
+                    .customer(customers.get(randomCustomer))
+                    .books(getBooksForOrder(countOfBooks))
+                    .build();
+        };
+    }
+
+    private void populatePublishers() {
+        var publisherSupplier = publisherSupplier();
+        publishers = Stream.generate(publisherSupplier)
+                .limit(MAX_NUMBER_OF_PUBLISHERS)
+                .toList();
+    }
+
+    private void populateAuthors() {
+        var authorSupplier = authorSupplier();
+        authors = Stream.generate(authorSupplier)
+                .limit(MAX_NUMBER_OF_AUTHORS)
+                .toList();
+    }
+
+    private void populateBooks() {
+        var bookSupplier = bookSupplier();
+        books = Stream.generate(bookSupplier)
+                .limit(MAX_NUMBER_OF_BOOKS)
+                .toList();
+    }
+
+    private void populateCustomers() {
+        var customerSupplier = customerSupplier();
+        customers = Stream.generate(customerSupplier)
+                .limit(MAX_NUMBER_OF_CUSTOMERS)
+                .toList();
+    }
+
+    private void populateOrders() {
+        var orderSupplier = orderSupplier();
+        orders = Stream.generate(orderSupplier)
+                .limit(MAX_NUMBER_OF_ORDERS)
+                .toList();
+    }
+
+    private List<Book> getBooksForOrder(int countOfBooks) {
+        return Stream.generate(() -> {
+            int randomBook = faker.number().numberBetween(0, MAX_NUMBER_OF_BOOKS);
+            return books.get(randomBook);
+        }).limit(countOfBooks).toList();
+    }
+
+    private Set<Author> getAuthorsForBook() {
+        int numberOfAuthors = 1;
+
+        if (MAX_NUMBER_OF_AUTHORS_PER_BOOK > 1) {
+            numberOfAuthors = faker.number().numberBetween(1, MAX_NUMBER_OF_AUTHORS_PER_BOOK);
         }
 
-        log.info("Bootstrap is completed. Basic values in database.");
+        return Stream.generate(() -> {
+            int randomAuthor = faker.number().numberBetween(0, MAX_NUMBER_OF_AUTHORS);
+            return authors.get(randomAuthor);
+        }).limit(numberOfAuthors).collect(Collectors.toSet());
     }
 }
--- src/main/java/core/project/library/infrastructure/repository/BootstrapRepository.java
+++ src/main/java/core/project/library/infrastructure/repository/BootstrapRepository.java
@@ -1,29 +1,12 @@
 package core.project.library.infrastructure.repository;
 
 import core.project.library.domain.entities.*;
-import core.project.library.domain.events.Events;
-import core.project.library.domain.value_objects.*;
-import net.datafaker.Faker;
 import org.springframework.jdbc.core.JdbcTemplate;
-
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.UUID;
 
 @org.springframework.stereotype.Repository
 public class BootstrapRepository {
 
-    private static final Faker faker = new Faker();
-
     private final JdbcTemplate jdbcTemplate;
-
-    private static final Address DEFAULT_ADDRESS =
-            new Address("State", "City", "Street", "Home");
-
-    private static final BigDecimal DEFAULT_PRICE =
-            new BigDecimal("12.99");
 
     public BootstrapRepository(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
@@ -33,212 +16,12 @@ public class BootstrapRepository {
         return jdbcTemplate.queryForObject("Select count(*) from Book", Integer.class);
     }
 
-    public void bootstrap() {
-        Publisher publisher = Publisher.builder()
-                .id(UUID.randomUUID())
-                .publisherName(new PublisherName("Publisher"))
-                .address(DEFAULT_ADDRESS)
-                .phone(new Phone(faker.phoneNumber().phoneNumber()))
-                .email(new Email("email@gmail.com"))
-                .events(new Events())
-                .build();
-
-        Author author = Author.builder()
-                .id(UUID.randomUUID())
-                .firstName(new FirstName("Author"))
-                .lastName(new LastName("Authorovich"))
-                .email(new Email("author@gmail.com"))
-                .address(DEFAULT_ADDRESS)
-                .events(new Events())
-                .build();
-
-        Book book = Book.builder()
-                .id(UUID.fromString("d4f0aa27-317b-4e00-9462-9a7f0faa7a5e"))
-                .title(new Title("Title"))
-                .description(new Description("Description"))
-                .isbn(new ISBN("9781861972712"))
-                .price(DEFAULT_PRICE)
-                .quantityOnHand(43)
-                .events(new Events())
-                .category(Category.Adventure)
-                .publisher(publisher)
-                .authors(new HashSet<>(Collections.singleton(author)))
-                .build();
-
-        Customer customer = Customer.builder()
-                .id(UUID.fromString("58e9909b-742f-4cd0-b1a1-0e8689d0fcfd"))
-                .firstName(new FirstName("Customer"))
-                .lastName(new LastName("Customerovich"))
-                .password(new Password("password"))
-                .email(new Email("customer@gmail.com"))
-                .address(DEFAULT_ADDRESS)
-                .events(new Events())
-                .build();
-
-        Order order = Order.builder()
-                .id(UUID.fromString("a486f288-cec3-4205-b753-d4ddf2796f9a"))
-                .countOfBooks(1)
-                .totalPrice(new TotalPrice(DEFAULT_PRICE))
-                .events(new Events())
-                .customer(customer)
-                .books(new ArrayList<>(Collections.singleton(book)))
-                .build();
-
-        //-----------------------------------------------------------------------------------------------------------
-
-        Publisher publisher2 = Publisher.builder()
-                .id(UUID.randomUUID())
-                .publisherName(new PublisherName("Publisher2"))
-                .address(DEFAULT_ADDRESS)
-                .phone(new Phone(faker.phoneNumber().phoneNumber()))
-                .email(new Email("email@gmail.com"))
-                .events(new Events())
-                .build();
-
-        Author author2 = Author.builder()
-                .id(UUID.randomUUID())
-                .firstName(new FirstName("Author2"))
-                .lastName(new LastName("Authorovich"))
-                .email(new Email("author@gmail.com"))
-                .address(DEFAULT_ADDRESS)
-                .events(new Events())
-                .build();
-
-        Book book2 = Book.builder()
-                .id(UUID.fromString("9f16fea8-4482-406c-aa53-43593d1cb480"))
-                .title(new Title("Title2"))
-                .description(new Description("Description"))
-                .isbn(new ISBN("9781861972712"))
-                .price(DEFAULT_PRICE)
-                .quantityOnHand(43)
-                .events(new Events())
-                .category(Category.Adventure)
-                .publisher(publisher)
-                .authors(new HashSet<>(Collections.singleton(author2)))
-                .build();
-
-        Customer customer2 = Customer.builder()
-                .id(UUID.randomUUID())
-                .firstName(new FirstName("Customer2"))
-                .lastName(new LastName("Customerovich2"))
-                .password(new Password("password"))
-                .email(new Email("customer2@gmail.com"))
-                .address(DEFAULT_ADDRESS)
-                .events(new Events())
-                .build();
-
-        Order order2 = Order.builder()
-                .id(UUID.randomUUID())
-                .countOfBooks(1)
-                .totalPrice(new TotalPrice(DEFAULT_PRICE))
-                .events(new Events())
-                .customer(customer)
-                .books(new ArrayList<>(Collections.singleton(book)))
-                .build();
-
-        //-----------------------------------------------------------------------------------------------------------
-
-        savePublisher(publisher);
-        savePublisher(publisher2);
-
-        saveAuthor(author);
-        saveAuthor(author2);
-
-        saveBook(book);
-        saveBook(book2);
-
-        saveBookAuthor(book, author);
-        saveBookAuthor(book2, author2);
-
-        saveCustomer(customer);
-        saveCustomer(customer2);
-
-        saveOrder(order);
-        saveOrder(order2);
-
-        saveBookOrder(book, order);
-        saveBookOrder(book2, order2);
-
-        //-----------------------------------------------------------------------------------------------------------
-
-        /**Publisher publisher3 = Publisher.builder()
-                .id(UUID.randomUUID())
-                .publisherName(new PublisherName("PublisherForRow"))
-                .address(DEFAULT_ADDRESS)
-                .phone(new Phone("+994 50 1112233"))
-                .email(new Email("email@gmail.com"))
-                .events(new Events())
-                .build();
-
-        Author author3 = Author.builder()
-                .id(UUID.randomUUID())
-                .firstName(new FirstName("AuthorForRow"))
-                .lastName(new LastName("Authorovich"))
-                .email(new Email("author@gmail.com"))
-                .address(DEFAULT_ADDRESS)
-                .events(new Events())
-                .build();
-
-        Author author4 = Author.builder()
-                .id(UUID.randomUUID())
-                .firstName(new FirstName("SecondAuthorForRow"))
-                .lastName(new LastName("Authorovich"))
-                .email(new Email("author@gmail.com"))
-                .address(DEFAULT_ADDRESS)
-                .events(new Events())
-                .build();
-
-        Book book3 = Book.builder()
-                .id(UUID.fromString("b77380fb-ecde-41b3-ab6b-9cc935ca2d1c"))
-                .title(new Title("BookForRow"))
-                .description(new Description("Description"))
-                .isbn(new ISBN("9781861972712"))
-                .price(DEFAULT_PRICE)
-                .quantityOnHand(43)
-                .events(new Events())
-                .category(Category.Adventure)
-                .publisher(publisher3)
-                .authors(new HashSet<>(Set.of(author3, author4)))
-                .build();
-
-        Customer customer3 = Customer.builder()
-                .id(UUID.randomUUID())
-                .firstName(new FirstName("CustomerForRow"))
-                .lastName(new LastName("Customerovich"))
-                .password(new Password("password"))
-                .email(new Email("customer@gmail.com"))
-                .address(DEFAULT_ADDRESS)
-                .events(new Events())
-                .build();
-
-        Order order3 = Order.builder()
-                .id(UUID.randomUUID())
-                .countOfBooks(1)
-                .totalPrice(new TotalPrice(DEFAULT_PRICE))
-                .events(new Events())
-                .customer(customer3)
-                .books(new HashSet<>(Collections.singleton(book3)))
-                .build();
-
-        //-----------------------------------------------------------------------------------------------------------
-
-        savePublisher(publisher3);
-        saveAuthor(author3);
-        saveAuthor(author4);
-        saveBook(book3);
-        saveBookAuthor(book3, author3);
-        saveBookAuthor(book3, author4);
-        saveCustomer(customer3);
-        saveOrder(order3);
-        saveBookOrder(book3, order3);*/
-    }
-
     private void savePublisher(Publisher publisher) {
         jdbcTemplate.update("""
-        Insert into Publishers (id, publisher_name, state, city, street, home,
-                       phone, email, creation_date, last_modified_date)
-                       values (?,?,?,?,?,?,?,?,?,?)
-        """,
+                        Insert into Publishers (id, publisher_name, state, city, street, home,
+                                       phone, email, creation_date, last_modified_date)
+                                       values (?,?,?,?,?,?,?,?,?,?)
+                        """,
                 publisher.getId().toString(), publisher.getPublisherName().publisherName(),
                 publisher.getAddress().state(), publisher.getAddress().city(),
                 publisher.getAddress().street(), publisher.getAddress().home(),
@@ -249,10 +32,10 @@ public class BootstrapRepository {
 
     private void saveAuthor(Author author) {
         jdbcTemplate.update("""
-        Insert into Authors (id, first_name, last_name, email,
-                    state, city, street, home, creation_date, last_modified_date)
-                    values (?,?,?,?,?,?,?,?,?,?)
-        """,
+                        Insert into Authors (id, first_name, last_name, email,
+                                    state, city, street, home, creation_date, last_modified_date)
+                                    values (?,?,?,?,?,?,?,?,?,?)
+                        """,
                 author.getId().toString(), author.getFirstName().firstName(), author.getLastName().lastName(),
                 author.getEmail().email(), author.getAddress().state(), author.getAddress().city(),
                 author.getAddress().street(), author.getAddress().home(),
@@ -262,10 +45,10 @@ public class BootstrapRepository {
 
     private void saveBook(Book book) {
         jdbcTemplate.update("""
-        Insert into Books (id, publisher_id, title, description, isbn, price,
-                  quantity_on_hand, category, creation_date, last_modified_date)
-                  values (?,?,?,?,?,?,?,?,?,?)
-        """,
+                        Insert into Books (id, publisher_id, title, description, isbn, price,
+                                  quantity_on_hand, category, creation_date, last_modified_date)
+                                  values (?,?,?,?,?,?,?,?,?,?)
+                        """,
                 book.getId().toString(), book.getPublisher().getId().toString(),
                 book.getTitle().title(), book.getDescription().description(),
                 book.getIsbn().isbn(), book.getPrice(), book.getQuantityOnHand(),
@@ -276,9 +59,9 @@ public class BootstrapRepository {
 
     private void saveBookAuthor(Book book, Author author) {
         jdbcTemplate.update("""
-        Insert into Book_Author (book_id, author_id)
-                    values (?,?,?)
-        """,
+                        Insert into Book_Author (book_id, author_id)
+                                    values (?,?,?)
+                        """,
                 book.getId().toString(),
                 author.getId().toString()
         );
@@ -286,11 +69,11 @@ public class BootstrapRepository {
 
     private void saveCustomer(Customer customer) {
         jdbcTemplate.update("""
-        Insert into Customers (id, first_name, last_name, email, password,
-                      state, city, street, home,
-                      creation_date, last_modified_date)
-                      values (?,?,?,?,?,?,?,?,?,?,?)
-        """,
+                        Insert into Customers (id, first_name, last_name, email, password,
+                                      state, city, street, home,
+                                      creation_date, last_modified_date)
+                                      values (?,?,?,?,?,?,?,?,?,?,?)
+                        """,
                 customer.getId().toString(), customer.getFirstName().firstName(), customer.getLastName().lastName(),
                 customer.getEmail().email(), customer.getPassword().password(), customer.getAddress().state(),
                 customer.getAddress().city(), customer.getAddress().street(), customer.getAddress().home(),
@@ -300,11 +83,11 @@ public class BootstrapRepository {
 
     private void saveOrder(Order order) {
         jdbcTemplate.update("""
-        Insert into Orders (id, customer_id,
-                        count_of_book, total_price,
-                        creation_date, last_modified_date)
-                        values (?,?,?,?,?,?)
-        """,
+                        Insert into Orders (id, customer_id,
+                                        count_of_book, total_price,
+                                        creation_date, last_modified_date)
+                                        values (?,?,?,?,?,?)
+                        """,
                 order.getId().toString(), order.getCustomer().getId().toString(),
                 order.getCountOfBooks(), order.getTotalPrice().totalPrice(),
                 order.getEvents().creation_date(), order.getEvents().last_update_date()
@@ -313,9 +96,9 @@ public class BootstrapRepository {
 
     private void saveBookOrder(Book book, Order order) {
         jdbcTemplate.update("""
-        Insert into Book_Order (book_id, order_id)
-                    values (?,?,?)
-        """,
+                        Insert into Book_Order (book_id, order_id)
+                                    values (?,?,?)
+                        """,
                 book.getId().toString(),
                 order.getId().toString()
         );
