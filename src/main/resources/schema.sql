70f399c575831801d0bed0efe58c63fb5685aff8
Ain Grace <qnicat32@gmail.com>
2024-07-04 16:52:13 +0400

    Update schema.sql for H2 and delete BootstrapTest.java

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -10,12 +10,13 @@ create table Publishers (
                             email varchar(255) not null,
                             creation_date timestamp not null,
                             last_modified_date timestamp not null,
-                            unique (phone),
-                            unique (email),
+--                             unique (phone),
+--                             unique (email),
                             primary key (id)
 );
 create unique index publisher_email_index on Publishers(email);
 create unique index publisher_phone_index on Publishers(phone);
+
 drop table if exists Authors;
 create table Authors (
                          id varchar(36) not null,
@@ -28,10 +29,11 @@ create table Authors (
                          home varchar(51) not null,
                          creation_date timestamp not null,
                          last_modified_date timestamp not null,
-                         unique (email),
+--                          unique (email),
                          primary key (id)
 );
 create unique index author_email_index on Authors(email);
+
 drop table if exists Books;
 drop table if exists Book_Author;
 create table Books (
@@ -45,12 +47,11 @@ create table Books (
                        category varchar(255) not null,
                        creation_date timestamp not null,
                        last_modified_date timestamp not null,
-                       unique (isbn),
+--                        unique (isbn),
                        primary key (id),
                        constraint book_publisher_fk foreign key (publisher_id) references Publishers (id)
 );
 create unique index isbn_index on Books(isbn);
-drop  table if exists Book_Author;
 create table Book_Author (
                              book_id varchar(36) not null,
                              author_id varchar(36) not null,
@@ -58,6 +59,7 @@ create table Book_Author (
                              constraint book_author_fk foreign key (book_id) references Books (id),
                              constraint author_book_fk foreign key (author_id) references Authors (id)
 );
+
 drop table if exists Customers;
 create table Customers (
                            id varchar(36) not null,
@@ -71,27 +73,35 @@ create table Customers (
                            home varchar(51) not null,
                            creation_date timestamp not null,
                            last_modified_date timestamp not null,
-                           unique (email),
+--                            unique (email),
                            primary key (id)
 );
 create unique index customer_email on Customers(email);
+
 drop table if exists Orders;
-create table Orders (
+Create table Orders (
                         id varchar(36) not null,
                         customer_id varchar(36) not null,
                         count_of_book int not null,
                         total_price decimal(38, 2) not null,
+                        paid_amount decimal(38, 2) not null,
+                        change_of_order decimal(38, 2) not null,
+                        credit_card_number char(16) not null,
+                        credit_card_expiration char(10) not null,
                         creation_date timestamp not null,
-                        last_modified_date timestamp not null,
-                        primary key (id),
-                        constraint customer_order_fk foreign key (customer_id) references Customers (id)
+                        Primary key (id),
+                        constraint customer_order_fk
+                            foreign key (customer_id) references Customers (id)
 );
-drop table if exists Book_Order;
-create table Book_Order (
+
+-- drop table if exists Book_Order;
+Create table Book_Order (
                             book_id varchar(36) not null,
                             order_id varchar(36) not null,
-                            book_count integer not null,
+                            count_of_book_copies integer not null,
                             primary key (book_id, order_id),
-                            constraint book_order_fk foreign key (book_id) references Books (id),
-                            constraint order_book_fk foreign key (order_id) references Orders (id)
+                            constraint book_order_fk
+                                foreign key (book_id) references Books (id),
+                            constraint order_book_fk
+                                foreign key (order_id) references Orders (id)
 );
