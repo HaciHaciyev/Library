49583147a131dae1cd42bc15759a20ffa744eea3
Ain Grace <qnicat32@gmail.com>
2024-07-04 18:55:17 +0400

    Replace JdbcTemplate with JdbcClient

--- src/test/java/core/project/library/repository/AuthorRepositoryTest.java
+++ src/test/java/core/project/library/repository/AuthorRepositoryTest.java
@@ -3,9 +3,11 @@ package core.project.library.repository;
 import core.project.library.application.bootstrap.Bootstrap;
 import core.project.library.domain.entities.Author;
 import core.project.library.domain.events.Events;
+import core.project.library.domain.value_objects.LastName;
 import core.project.library.infrastructure.repository.AuthorRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -16,9 +18,13 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementSetter;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.simple.JdbcClient;
 
+import java.util.List;
 import java.util.UUID;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
@@ -39,8 +45,8 @@ class AuthorRepositoryTest {
     @TestConfiguration
     static class TestConfig {
         @Bean
-        AuthorRepository authorRepository(JdbcTemplate jdbcTemplate) {
-            return new AuthorRepository(jdbcTemplate);
+        AuthorRepository authorRepository(JdbcClient jdbcClient) {
+            return new AuthorRepository(jdbcClient);
         }
     }
 
@@ -54,7 +60,7 @@ class AuthorRepositoryTest {
                 .events(new Events())
                 .build();
 
-        return Stream.generate(() -> arguments(author.get())).limit(5);
+        return Stream.generate(() -> arguments(author.get())).limit(1);
     }
 
 
@@ -72,11 +78,60 @@ class AuthorRepositoryTest {
     @MethodSource("author")
     @DisplayName("reject invalid author")
     void rejectInvalidAuthor(Author author) {
-        when(jdbcTemplate.update(anyString(), any(PreparedStatementSetter.class)))
+        when(jdbcTemplate.update(any(PreparedStatementCreator.class)))
                 .thenThrow(DataIntegrityViolationException.class);
 
         assertThat(authorRepository.saveAuthor(author).throwable())
                 .isNotNull()
                 .isInstanceOf(DataAccessException.class);
+    }
+
+    @ParameterizedTest
+    @MethodSource("author")
+    @DisplayName("FindById test")
+    void findById(Author author) {
+        when(jdbcTemplate.query(anyString(), any(RowMapper.class), any())).thenReturn(List.of(author));
+
+        assertThat(authorRepository.findById(author.getId()).value())
+                .isNotNull()
+                .isEqualTo(author);
+    }
+
+    @Test
+    @DisplayName("reject invalid id")
+    void rejectInvalidId() {
+        when(jdbcTemplate.queryForObject(anyString(), any(RowMapper.class), any()))
+                .thenThrow(EmptyResultDataAccessException.class);
+
+        assertThat(authorRepository.findById(UUID.randomUUID()).throwable())
+                .isNotNull()
+                .isInstanceOf(EmptyResultDataAccessException.class);
+    }
+
+    private static Stream<Arguments> authorName() {
+        String lastName = "Williams";
+
+        Supplier<Author> author =() -> Author.builder()
+                .id(UUID.randomUUID())
+                .firstName(Bootstrap.randomFirstName())
+                .lastName(new LastName(lastName))
+                .email(Bootstrap.randomEmail())
+                .address(Bootstrap.randomAddress())
+                .events(new Events())
+                .build();
+
+        List<Author> authors = Stream.generate(author).limit(5).toList();
+
+        return Stream.generate(() -> arguments(authors, lastName)).limit(1);
+    }
+
+    @ParameterizedTest
+    @MethodSource("authorName")
+    @DisplayName("FindByLastName test")
+    void findByLastName(List<Author> authors, String lastName) {
+        when(jdbcTemplate.query(anyString(), any(RowMapper.class), any())).thenReturn(authors);
+
+        assertThat(authorRepository.findByLastName(lastName).value())
+                .allMatch(author -> author.getLastName().lastName().equals(lastName));
     }
 }
